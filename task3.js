3f6cc7162ca2a1dc85c0ce77a8d991f9c78f0375
Jo-Villa <josefina.villalobos@genstudents.org>
2025-04-15 16:08:43 +0000

    Tarea terminada

--- task3.js
+++ task3.js
@@ -1,8 +1,15 @@
 export class Player {
-  constructor() {
-  
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
   }
 
-
-
+info() {
+  console.log(`¡${this.name} ha alcanzado el nivel ${this.level}!`);
+  }
 }
+
+//Luego, defina un método de objeto compartido info() que imprimirá la siguiente cadena, reemplazando los
+// dos marcadores de posición:
+//§ <name> has reached Level <level>!
+//· Un Player llamado Tara en el nivel 6 debería imprimir " Tara has reached Level 6!" en la consola.
--- task4.js
+++ task4.js
@@ -1,7 +1,19 @@
+//Ahora se te ha pedido que incluyas un método para subir de nivel a un Player, aumentando su número de nivel en uno.
+//Modifique la clase Player para que acepte una cadena de name de Player y un número de nivel en dos argumentos separados.
+//Luego, defina un método de objeto compartido info()que generará la siguiente cadena:
+//<name> has reached Level <level>!
+
 export class Player {
-  constructor() {
-   
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
   }
-  
-  
+
+info() {
+  console.log(`¡${this.name} ha alcanzado el nivel ${this.level}!`);
+  }
+
+  levelUp() {
+    this.level += 1;
+  }
 }
